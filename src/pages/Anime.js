2595739434420c71abd13f8a23e924195a44f0a1
SiddharthThe <siddharthvhatkar4@gmail.com>
2025-04-24 19:45:17 +0530

    fixed anime page

--- src/pages/Anime.js
+++ src/pages/Anime.js
@@ -7,7 +7,7 @@ const Anime = () => {
         {
             title: 'Attack on Titan',
             year: '2013',
-            image: 'https://www.anime-planet.com/images/anime/cover/1.jpg',
+            image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSgVLwmeJQBHNxGE8AMaCGWQLWoCUZMFJvuJy0nKdDfSnSXITF69Ivwb1qqnZNgyloaCKPo',
             description: 'In a world where humanity resides within enormous walled cities to protect themselves from Titans, gigantic humanoid creatures, the story follows Eren Yeager and his friends who join the military to fight against the Titans.',
         },
         {
@@ -20,15 +20,15 @@ const Anime = () => {
 
     return (
         <div className="anime-page">
-            <h1>Movies</h1>
+            
             <div className="anime-grid">
                 {animes.map((anime, index) => (
                     <AnimeCard
                         key={index}
-                        title={animes.title}
-                        year={animes.year}
-                        image={animes.image}
-                        description={animes.description}
+                        title={anime.title}
+                        year={anime.year}
+                        image={anime.image}
+                        description={anime.description}
                     />
                 ))}
             </div>
