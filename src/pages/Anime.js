b7b5710d580b94be81732591d8d43c69e901121c
HA24RSH <harshvardhangadagade24555@gmail.com>
2025-04-24 17:08:01 +0530

    started work on anime page

--- src/pages/Anime.js
+++ src/pages/Anime.js
@@ -3,7 +3,15 @@ import react from 'react';
 const Anime = () => {
     return (
         <div className='anime-page'>
-            <h1>Anime Page</h1>
+            <h1>Top Rating Animes</h1>
+            <div className='anime-content'>
+                <div className='anime-card'>
+                    <img src='https://www.anime-planet.com/images/anime/cover/1.jpg' alt='Anime 1' />
+                    <h2>Attack on Titan</h2>
+                    <p>Rating: 9.5</p>
+                    
+                </div>
+            </div>
         </div>
     )
 }
